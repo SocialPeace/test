3f17caed5274b4ebac0d4587d9c1fc1a608efc8c
jeongin <12183385@inha.edu>
2021-04-14 12:22:07 +0900

    test:fightvideo_extract_diff.py modify / ji

--- opencv/fightvideo_extract_diff.py
+++ opencv/fightvideo_extract_diff.py
@@ -3,15 +3,8 @@ import os
 
 from xml.etree.ElementTree import parse
 
-
-# 트랙커 객체 생성자 함수
-
-
-labels = parse('fight.xml')
-root = labels.getroot()
-
-
-labels = parse('fight.xml')
+################# xml parsing #################
+labels = parse('fight.xml')  # xml 넣는 곳
 root = labels.getroot()
 
 
@@ -31,29 +24,44 @@ start_point = (start_minu + (start_sec//100))*standard_frame
 dur_minu = int(durations[0].split(':')[1])
 dur_sec = int(float(durations[0].split(':')[2]))
 
-end_point = start_point + (dur_minu + (dur_sec//100))*standard_frame
+end_point = (dur_minu + (dur_sec//100))*standard_frame
+
 
 objs = root.findall('object')
 positions = [x.findall('position') for x in objs]
-actions = [x.findall('action') for x in objs]
 
 
-x1, x2, _y, w_, h = (9999, 0, 0, 0, 0)
-
-for pos in positions:
-    keyframe = [x.findtext('keyframe') for x in pos][0]
-    keypoint = [x.findall('keypoint') for x in pos][0]
-
-    x = int([x.findtext('x') for x in keypoint][0])
-    y = int([x.findtext('y') for x in keypoint][0])
-    x1 = min(x1, x)
-    x2 = max(x2, x)
-
-    _y = max(y, y)
+actions = []
+for obj in objs:
+    actions += obj.findall('action')
 
 
-w = x1 - x2
-h = 100  # 일단 임의로
+# 각 액션 시작, 끝 프레임 추출
+starts = []
+ends = []
+abnoraml_frame_total = 0
+action_frames = [x.findall('frame') for x in actions]
+
+
+print("=============== actions frames ============")
+print("start end")
+print("===========================================")
+for frames in action_frames:
+    for frame in frames:
+        start = int(frame.findtext('start'))
+        end = int(frame.findtext('end'))
+        abnoraml_frame_total += end-start
+        print(start, end)
+        starts.append(start)
+        ends.append(end)
+
+
+print(abnoraml_frame_total)
+
+
+print('start_frame : %d , end_frame:  %d ' % (start_point, end_point))
+
+##################### video parsing ######################
 
 # 비디오에서 프레임 추출
 img_last = None  # 이전 프레임을 저장할 변수
@@ -61,37 +69,41 @@ no = 0  # 이미지 장 수
 save_dir = "./fight"  # 저장 디렉토리 이름
 os.mkdir(save_dir)  # 디렉토리
 
-
-print(start_point, end_point)
-# # 동영상 파일로부터 입력받기 -- (*1)
-
 frame_cnt = 0
-cap = cv2.VideoCapture("fight.mp4")
-cap.set(1, start_point)
-
-while True:
-    # 이미지 추출하기
-    is_ok, frame = cap.read(start_point)
-    frame = cv2.resize(frame, (640, 800))
-
-    if not is_ok:
-        break
-
-    frame_cnt += 1
-
-    print(frame_cnt)
-    # 이상행동 끝나는 지점
-    if frame_cnt > end_point-start_point:
-        break
-
-    # 이상행동 시작하는 지점
-    # if frame_cnt >= start_point:
-    roi = frame[0:_y, x:x+w]
-    cv2.rectangle(roi, (0, 0), (h-1, w-1), (0, 255, 0))
-    outfile = save_dir + "/" + str(no) + ".jpg"
-    cv2.imwrite(outfile, frame)
-    no += 1
+cap = cv2.VideoCapture("fight.mp4")  # mp4 파일 넣는 곳
 
 
-cap.release()
-print("ok")
+for start, end in zip(starts, ends):
+    diff = end-start
+    print(diff)
+    frame_cnt = 0
+    while True:
+        cap.set(1, start)  # 이상행동 시작 구간으로 프레임 세팅
+        # 이미지 추출하기
+        is_ok, frame = cap.read()  # 프레임 읽기
+        frame = cv2.resize(frame, (640, 800))  # 프레임 리사이징
+
+        if not is_ok:
+            break
+
+        frame_cnt += 1
+        print(start)
+
+        # 이상행동 끝나는 지점
+        if start >= end:
+            print("frame_end")
+            break
+
+        # 프레임 이미지로 저장
+        # 액션이 나오는 프레임만 자르기
+        outfile = save_dir + "/" + str(no) + ".jpg"
+        cv2.imwrite(outfile, frame)
+        no += 1
+        total = no
+        start += diff // 2
+
+
+print("===============================")
+print('total : %d' % (no))
+print('actions : %d' % (len(actions)))
+print("===============================")
